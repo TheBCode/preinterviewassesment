96d33e03d74573f8a1b08f65bd5d0696dd20fddf
Byron Buyck <bbuyck1@gmail.com>
2019-03-26 19:57:20 -0400

    Added json file, used fetch to pull in json data and store it in array that can be used with jsx, tested card w data

--- src/App.js
+++ src/App.js
@@ -3,18 +3,49 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [],
+      isLoaded: false,
+
+    }
+  }
+
+  componentDidMount() {
+    fetch('UIE-InterviewProject.json')
+        .then(res => res.json())
+        .then(json => {
+          this.setState({
+            isLoaded: true,
+            items: json,
+
+          })
+        })
+  }
+
   render() {
-    return (
-      <div className="App container">
-        <div className="card">
-          <img className="card-img-top" src="/public/darktiles_bg_gike55.jpg" alt=""/>
-          <div className="card-body">
-            <p className="card-text">Some quick example text to build on the card title and
-              make up the bulk of the card's content</p>
+    var {isLoaded, items} = this.state;
+    console.log(items)
+    if (!isLoaded) {
+      return <div>Loading...</div>
+    } else {
+      return (
+
+          <div className="App container">
+            <div className="card">
+              <img className="card-img-top" src="darktiles_bg_gike55.jpg" alt="Card image cap"/>
+              <div className="card-body">
+                <h5 className="card-title">{items[0].Heading}</h5>
+                <h6 className="card-subtitle mb-2 text-muted">{items[0].Subheading}</h6>
+                <hr/>
+                <p className="card-text text-success">{items[0].Price}</p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    );
+
+      );
+    }
   }
 }
 
