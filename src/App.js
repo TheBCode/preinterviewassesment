2c009a8d83902e05525bd520b5ac4bf58da3a709
Byron Buyck <bbuyck1@gmail.com>
2019-03-26 20:54:36 -0400

    Fixed styling with card-img-tops

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -32,10 +31,13 @@ class App extends Component {
     } else {
       return (
 
-          <div className="App container card-columns">
-              {items.map(item => (
+          <div className="App container">
+
+              <div className="row">{items.map(item => (
+
                   <div key={item.index} className="card">
                     <img className="card-img-top" src="darktiles_bg_gike55.jpg" alt="Card image cap"/>
+                    <img className="card-img-top img-overlay" src="place_holder_zuvywg.png"/>
                     <div className="card-body">
                       <h5 className="card-title">{item.Heading}</h5>
                       <h6 className="card-subtitle mb-2 text-muted">{item.Subheading}</h6>
@@ -43,7 +45,7 @@ class App extends Component {
                       <p className="card-text text-success text-right">{item.Price}</p>
                     </div>
                   </div>
-              ))}
+              ))}</div>
 
           </div>
       );
